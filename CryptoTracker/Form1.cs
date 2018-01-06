2c333ad182865007f66c2765db21a64feaf6136a
willem445 <willh445447@hotmail.com>
2018-01-06 09:41:34 -0600

    Added project files

---
+++ CryptoTracker/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Timers;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace CryptoTracker
+{
+    public partial class Form1 : Form
+    {
+        PriceManager priceManager;
+        List<Label> labelPriceList = new List<Label>();
+        private TextBox[,] textboxes;
+        System.Timers.Timer updatePrices;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Crypto Tracker";
+
+            priceManager = new PriceManager();
+
+            labelPriceList.Add(bitcoinLabel);
+            labelPriceList.Add(etherLabel);
+            labelPriceList.Add(litecoinLabel);
+            labelPriceList.Add(stellarLabel);
+            labelPriceList.Add(rippleLabel);
+            labelPriceList.Add(iotaLabel);
+            labelPriceList.Add(waltonLabel);
+            labelPriceList.Add(reqLabel);
+
+            labelPriceList.Add(iconLabel);
+            labelPriceList.Add(vechainLabel);
+            labelPriceList.Add(binanceLabel);
+            labelPriceList.Add(perlLabel);
+            labelPriceList.Add(bntyLabel);
+            labelPriceList.Add(dentLabel);
+            labelPriceList.Add(linkLabel);
+
+            textboxes = new TextBox[,] 
+            {
+                {bitcoinQuantityTB, bitcoinInvestedTB, bitcoinValueTB, bitcoinProfitTB, bitcoinProfitPercentTB},
+                {etherQuantityTB, etherInvestedTB, etherValueTB, etherProfitTB, etherProfitPercentTB},
+                {litecoinQuantityTB, litecoinInvestedTB, litecoinValueTB, litecoinProfitTB, litecoinProfitPercentTB},
+                {stellarQuantityTB, stellarInvestedTB, stellarValueTB, stellarProfitTB, stellarProfitPercentTB},
+                {rippleQuantityTB, rippleInvestedTB, rippleValueTB, rippleProfitTB, rippleProfitPercentTB},
+                {iotaQuantityTB, iotaInvestedTB, iotaValueTB, iotaProfitTB, iotaProfitPercentTB},
+                {waltonQuantityTB, waltonInvestedTB, waltonValueTB, waltonProfitTB, waltonProfitPercentTB},
+                {reqQuantityTB, reqInvestedTB, reqValueTB, reqProfitTB, reqProfitPercentTB},
+
+                {iconQuantityTB, iconInvestedTB, iconValueTB, iconProfitTB, iconProfitPercentTB},
+                {vechainQuantityTB, vechainInvestedTB, vechainValueTB, vechainProfitTB, vechainProfitPercentTB},
+                {binanceQuantityTB, binanceInvestedTB, binanceValueTB, binanceProfitTB, binanceProfitPercentTB},
+                {perlQuantityTB, perlInvestedTB, perlValueTB, perlProfitTB, perlProfitPercentTB},
+                {bntyQuantityTB, bntyInvestedTB, bntyValueTB, bntyProfitTB, bntyProfitPercentTB},
+                {dentQuantityTB, dentInvestedTB, dentValueTB, dentProfitTB, dentProfitPercentTB},
+                {linkQuantityTB, linkInvestedTB, linkValueTB, linkProfitTB, linkProfitPercentTB}
+            };
+
+            updatePrices = new System.Timers.Timer();
+            updatePrices.Interval = 30000; //30 seconds
+            updatePrices.Elapsed += new ElapsedEventHandler(UpdatePrices);
+            updatePrices.Start();
+        }
+
+        public void UpdatePrices(object sender, ElapsedEventArgs e)
+        {
+            priceManager.UpdatePriceData();
+            UpdateUI();
+        }
+
+        //Refresh data
+        private void button1_Click(object sender, EventArgs e)
+        {
+            priceManager.UpdatePriceData();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            int i = 0;
+            foreach (var item in labelPriceList)
+            {
+                this.Invoke((MethodInvoker)delegate {
+                    //if (priceManager.coinPrice[i] < Convert.ToDouble(item.Text.Replace('$', ' ')))
+                    //{
+                    //    item.ForeColor = Color.Red;
+                    //}
+                    //else if (priceManager.coinPrice[i] > Convert.ToDouble(item.Text.Replace('$', ' ')))
+                    //{
+                    //    item.ForeColor = Color.Green;
+                    //}
+                    //else
+                    //{
+                    //    item.ForeColor = Color.Black;
+                    //}
+
+                    item.Text = "$" + priceManager.coinPrice[i].ToString("0.00"); // runs on UI thread
+                });
+
+                for (int j = 0; j < 5; j++)
+                {
+                    if (j == 0)
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            textboxes[i, j].Text = priceManager.valueArray[i, j].ToString("0.000000"); // runs on UI thread
+                        });
+                    }
+                        
+                    else if (j >= 1 && j <= 3)
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            textboxes[i, j].Text = "$" + priceManager.valueArray[i, j].ToString("0.00"); // runs on UI thread
+                        });
+                    }
+                        
+                    else if (j == 4)
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            textboxes[i, j].Text = priceManager.valueArray[i, j].ToString("0.00") + "%"; // runs on UI thread
+                        });
+                    }
+
+                    if (j == 3)
+                    {
+                        this.Invoke((MethodInvoker)delegate {
+                            if (priceManager.valueArray[i, j] <= 0)
+                            {
+                                textboxes[i, j].ForeColor = Color.Red; // runs on UI thread
+                            }
+                            else
+                            {
+                                textboxes[i, j].ForeColor = Color.Green; // runs on UI thread
+                            }
+                        });
+                    }
+                        
+                }
+
+                i++;
+            }
+
+            this.Invoke((MethodInvoker)delegate {
+                totalProfitLabel.Text = "$" + priceManager.totalProfit.ToString("0.00"); // runs on UI thread
+                totalInvestedLabel.Text = "$" + priceManager.totalInvestment.ToString("0.00");
+                totalValueLabel.Text = "$" + priceManager.totalValue.ToString("0.00");
+            });
+        }
+
+        //Open saved file
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                priceManager.ParseSavedData(openFileDialog1.FileName);
+            }
+
+            priceManager.UpdatePriceData();
+            UpdateUI();
+        }
+    }
+}
---
+++ CryptoTracker/PriceManager.cs
@@ -0,0 +1,149 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CryptoTracker
+{
+    class PriceManager
+    {
+        enum rowNames
+        {
+            Quantity,
+            TotalInvested,
+            Value,
+            Profit,
+            ProfitPercent
+        }
+
+        string[] coins =
+        {
+            "https://api.coinmarketcap.com/v1/ticker/bitcoin/",
+            "https://api.coinmarketcap.com/v1/ticker/ethereum/",
+            "https://api.coinmarketcap.com/v1/ticker/litecoin/",
+            "https://api.coinmarketcap.com/v1/ticker/stellar/",
+            "https://api.coinmarketcap.com/v1/ticker/ripple/",
+            "https://api.coinmarketcap.com/v1/ticker/iota/",
+            "https://api.coinmarketcap.com/v1/ticker/walton/",
+            "https://api.coinmarketcap.com/v1/ticker/request-network/",
+
+            "https://api.coinmarketcap.com/v1/ticker/icon/",
+            "https://api.coinmarketcap.com/v1/ticker/vechain/",
+            "https://api.coinmarketcap.com/v1/ticker/binance-coin/",
+            "https://api.coinmarketcap.com/v1/ticker/oyster-pearl/",
+            "https://api.coinmarketcap.com/v1/ticker/bounty0x/",
+            "https://api.coinmarketcap.com/v1/ticker/dent/",
+            "https://api.coinmarketcap.com/v1/ticker/chainlink/"
+        };
+
+        public float[,] valueArray =
+        {
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F},
+            { 0.0F, 0.0F, 0.0F, 0.0F, 0.0F}
+        };
+
+        public float[] coinPrice = new float[15];
+
+        public float totalProfit = 0.0F;
+        public float totalValue = 0.0F;
+        public float totalInvestment = 0.0F;
+
+        public void ParseSavedData(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            int i = 0;
+            foreach (string line in lines)
+            {
+                string[] data = line.Split(',');
+                valueArray[i, (int)rowNames.Quantity] = (float)Convert.ToDouble(data[1]);
+                valueArray[i, (int)rowNames.TotalInvested] = (float)Convert.ToDouble(data[2]);
+                i++;
+            }
+        }
+
+        public void UpdatePriceData()
+        {
+            APIUpdate();
+            CalculateValue();
+            CalculateProfit();
+            CalculateTotalProfit();
+        }
+
+
+        private void APIUpdate()
+        {
+            //Read data from API
+            for (int i = 0; i < coins.Length; i++)
+            {
+                string input = coins[i];
+
+                try
+                {
+                    var cli = new System.Net.WebClient();
+                    string prices = cli.DownloadString(input);
+
+                    dynamic results = JsonConvert.DeserializeObject<dynamic>(prices);
+
+                    coinPrice[i] = (float)(Convert.ToDouble(results[0].price_usd));
+                }
+                catch
+                {
+                    MessageBox.Show("Count not connect to API");
+                }
+
+            }
+        }
+
+        private void CalculateValue()
+        {
+            totalValue = 0.0F;
+            totalInvestment = 0.0F;
+            for (int i = 0; i < coins.Length; i++)
+            {
+                valueArray[i, (int)rowNames.Value] = valueArray[i, (int)rowNames.Quantity] * coinPrice[i];
+                totalInvestment += valueArray[i, (int)rowNames.TotalInvested];
+                totalValue += valueArray[i, (int)rowNames.Value];
+            }
+        }
+
+        private void CalculateProfit()
+        {
+            for (int i = 0; i < coins.Length; i++)
+            {
+                valueArray[i, (int)rowNames.Profit] = valueArray[i, (int)rowNames.Value] - valueArray[i, (int)rowNames.TotalInvested];
+                valueArray[i, (int)rowNames.ProfitPercent] = (valueArray[i, (int)rowNames.Profit] / valueArray[i, (int)rowNames.Value]) * 100;
+            }
+        }
+
+        private void CalculateTotalProfit()
+        {
+            totalProfit = 0.0F;
+            for (int i = 0; i < coins.Length; i++)
+            {
+                totalProfit += valueArray[i, (int)rowNames.Profit];
+            }
+        }
+
+
+
+
+    }
+}
