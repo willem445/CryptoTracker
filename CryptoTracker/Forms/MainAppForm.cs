b8295087655c5f9f07455e5ab13a9768580145d5
willem445 <willh445447@hotmail.com>
2018-01-31 21:18:33 -0600

    Added updateUI function that invokes updates to the form from the thread, fixes crashes from databinding

--- CryptoTracker/Forms/MainAppForm.cs
+++ CryptoTracker/Forms/MainAppForm.cs
@@ -132,6 +132,8 @@ namespace CryptoTracker
             {
                 AddNewCoinToFlowControl(item);
             }
+
+            UpdateUI();
         }
 
         private void ApplicationDataInitialize()
@@ -173,6 +175,7 @@ namespace CryptoTracker
 
             //Get data from API
            priceManager.UpdatePriceData();
+           UpdateUI();
         }
 
         /// <summary>
@@ -183,6 +186,33 @@ namespace CryptoTracker
         private void refreshButton_Click(object sender, MouseEventArgs e)
         {
             priceManager.UpdatePriceData();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            int i = 0;
+            foreach (var item in priceManager.CoinModelList)
+            {
+                priceLabelList[i].Invoke(new Action(() => priceLabelList[i].Text = item.PriceToString));
+
+                textBoxArrayList[i][0].Invoke(new Action(() => textBoxArrayList[i][0].Text = item.QuantityToString));
+                textBoxArrayList[i][1].Invoke(new Action(() => textBoxArrayList[i][1].Text = item.NetCostToString));
+                textBoxArrayList[i][2].Invoke(new Action(() => textBoxArrayList[i][2].Text = item.ValueToString));
+                textBoxArrayList[i][3].Invoke(new Action(() => textBoxArrayList[i][3].Text = item.ProfitToString));
+                textBoxArrayList[i][4].Invoke(new Action(() => textBoxArrayList[i][4].Text = item.ProfitPercentToString));
+
+                if (item.Profit.Value < 0)
+                {
+                    textBoxArrayList[i][3].Invoke(new Action(() => textBoxArrayList[i][3].ForeColor = Color.Red));
+                }
+                else
+                {
+                    textBoxArrayList[i][3].Invoke(new Action(() => textBoxArrayList[i][3].ForeColor = Color.Green));
+                }
+
+                i++;
+            }
         }
 
         /// <summary>
@@ -252,31 +282,29 @@ namespace CryptoTracker
 
             MetroFramework.Controls.MetroLabel coinPrice = new MetroFramework.Controls.MetroLabel();
             coinPrice.Name = addCoin.Name + "Label";
-            coinPrice.DataBindings.Add(new Binding("Text", addCoin, "PriceToString", false, DataSourceUpdateMode.OnPropertyChanged));
+
 
             toolTip.SetToolTip(coinPrice, "Test");
 
 
             MetroFramework.Controls.MetroTextBox coinQuantity = new MetroFramework.Controls.MetroTextBox();
             coinQuantity.Name = addCoin.Name + "Quantity_TB";
-            coinQuantity.DataBindings.Add("Text", addCoin, "QuantityToString", false, DataSourceUpdateMode.OnPropertyChanged);
+
 
             MetroFramework.Controls.MetroTextBox coinInvested = new MetroFramework.Controls.MetroTextBox();
             coinInvested.Name = addCoin.Name + "Invested_TB";
-            coinInvested.DataBindings.Add("Text", addCoin, "NetCostToString", false, DataSourceUpdateMode.OnPropertyChanged);
+
 
             MetroFramework.Controls.MetroTextBox coinValue = new MetroFramework.Controls.MetroTextBox();
             coinValue.Name = addCoin.Name + "Value_TB";
-            coinValue.DataBindings.Add("Text", addCoin, "ValueToString", false, DataSourceUpdateMode.OnPropertyChanged);
+
 
             MetroFramework.Controls.MetroTextBox coinProfit = new MetroFramework.Controls.MetroTextBox();
             coinProfit.Name = addCoin.Name + "Profit_TB";
-            coinProfit.DataBindings.Add("Text", addCoin, "ProfitToString", false, DataSourceUpdateMode.OnPropertyChanged);
-            coinProfit.DataBindings.Add("Forecolor", addCoin, "Color", false, DataSourceUpdateMode.OnPropertyChanged);
+
 
             MetroFramework.Controls.MetroTextBox coinProfitPercent = new MetroFramework.Controls.MetroTextBox();
             coinProfitPercent.Name = addCoin.Name + "ProfitPercent_TB";
-            coinProfitPercent.DataBindings.Add("Text", addCoin, "ProfitPercentToString", false, DataSourceUpdateMode.OnPropertyChanged);
 
             //Add controls to coin specifc flow panel
             newFlowPanel.Controls.Add(coinName);
