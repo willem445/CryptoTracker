674bccc2b0af7fbe6f0fcf0d193d801544fa4cc2
willem445 <willh445447@hotmail.com>
2018-02-04 10:45:22 -0600

    Added unit test project - need to update unit tests

--- CryptoTracker/Source/Import/BinanceImport.cs
+++ CryptoTracker/Source/Import/BinanceImport.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace CryptoTracker
 {
-    class BinanceImport : GeneralImport
+    public class BinanceImport : GeneralImport
     {
         //Enumerations***********************************************************************************
         enum BinanceColumns
